5c8f30617b9b1fcc55e15110267bd4b62dda0076
AngryPig123 <86225268+AngryPig123@users.noreply.github.com>
2026-01-01 01:59:02 +0900

    jpa, mybatis 설정 완료 (#1) (#2)
    jpa, mybatis 설정 완료 (#1)

---
+++ src/main/java/com/batchflow/config/MybatisConfig.java
@@ -0,0 +1,82 @@
+package com.batchflow.config;
+
+import java.util.stream.Stream;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.LocalCacheScope;
+import org.apache.ibatis.type.JdbcType;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.boot.autoconfigure.ConfigurationCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+
+/**
+ * packageName    : com.batchflow.config
+ * fileName       : MybatisConfig
+ * author         : AngryPig123
+ * date           : 26. 1. 1.
+ * description    :
+ * MyBatis 설정
+ * - Mapper 스캔(@MapperScan)
+ * - snake_case -> camelCase 매핑(mapUnderscoreToCamelCase)
+ * - TypeHandler 자동 스캔(typeHandlersPackage)
+ * - 배치/대량 처리에 유리한 캐시/타임아웃 기본값
+ *
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 26. 1. 1.        AngryPig123       최초 생성
+ */
+@Configuration
+@MapperScan(basePackages = {
+        "com.batchflow.domain.**.mapper",
+        "com.batchflow.batch.**.mapper"
+})
+public class MybatisConfig {
+
+    private static final String TYPE_HANDLERS_PACKAGE = "com.batchflow";
+
+    private static final String[] MAPPER_LOCATION_PATTERNS = {
+            "classpath*:mybatis/mapper/**/*.xml"
+    };
+
+    @Bean
+    public ConfigurationCustomizer mybatisConfigurationCustomizer() {
+        return configuration -> {
+            configuration.setMapUnderscoreToCamelCase(true);
+        };
+    }
+
+    @Bean
+    public org.apache.ibatis.session.SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setTypeHandlersPackage(TYPE_HANDLERS_PACKAGE);
+        factoryBean.setMapperLocations(resolveMapperLocations());
+        return factoryBean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(org.apache.ibatis.session.SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+
+    private org.springframework.core.io.Resource[] resolveMapperLocations() {
+        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        return Stream.of(MAPPER_LOCATION_PATTERNS)
+                .flatMap(pattern -> {
+                    try {
+                        return Stream.of(resolver.getResources(pattern));
+                    } catch (Exception e) {
+                        return Stream.empty();
+                    }
+                })
+                .toArray(org.springframework.core.io.Resource[]::new);
+    }
+
+}
