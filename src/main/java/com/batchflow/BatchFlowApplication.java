30bebddd9921310aad1d76e8f9983c3f5be1e732
Angrypig123 <shguddnr3@naver.com>
2026-01-01 00:19:54 +0900

    프로젝트 초기화

---
+++ src/main/java/com/batchflow/BatchFlowApplication.java
@@ -0,0 +1,13 @@
+package com.batchflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/batchflow/BatchFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batchflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
